309fa4639c8830995ed600ec550743dfe6006fad
PriyabrataP00 <priyabrata237@gmail.com>
2024-05-22 00:13:28 +0530

    rewardify_project

--- rewardify_project/settings.py
+++ rewardify_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rewardify-project.onrender.com']
+ALLOWED_HOSTS = ['rewardify-project.onrender.com','13.228.225.19','18.142.128.26','54.254.162.138']
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
