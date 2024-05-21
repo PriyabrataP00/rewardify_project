5f8b62ece46075a988c7d1316a784aa4855b4f8d
PriyabrataP00 <priyabrata237@gmail.com>
2024-05-21 23:56:56 +0530

    prod build

--- rewardify_project/settings.py
+++ rewardify_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rewardify-project.onrender.com']
 
@@ -104,9 +104,9 @@ WSGI_APPLICATION = 'rewardify_project.wsgi.application'
 
 
 
-DATABASES = {
-	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+# DATABASES = {
+# 	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 
 # Password validation
