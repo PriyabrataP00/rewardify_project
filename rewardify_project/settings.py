ffee2cd2629067c848d8a367b0a0004fe2a562db
Priyabrata Pradhan <159030325+PriyabrataP00@users.noreply.github.com>
2024-05-21 23:05:45 +0530

    Update settings.py

--- rewardify_project/settings.py
+++ rewardify_project/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+DEBUG = 'False'
 
-ALLOWED_HOSTS = ['rewardify-project..vercel.app','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['rewardify-project.vercel.app','localhost', '127.0.0.1']
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
